c321d421a80157c347bdcf98a6db2d7c82ac409c
Alexis-LB54 <alexis@lebail.eu>
2022-02-10 12:32:57 +0100

    commit ajout fichier

---
+++ vues/connexion.php
@@ -0,0 +1,33 @@
+
+        <div class="Formulaire" id="connexion">
+            <form method="POST">
+                
+                <h1>Connexion</h1>
+                
+                <div >
+                    <label for="email"> Email :</label>
+                </div>
+
+                <div >
+                    <input name="email" type="email" required />
+                </div>
+
+                <div >
+                    <label for="password">Mot de passe :</label>
+                </div>
+
+                <div >
+                    <input name="password" type="password" required />
+                </div>
+
+                <div>
+                <a href="#" class="bouttonbleu">Connexion</a>
+                </div>
+
+                <div>
+                    Pas de compte ?
+                    <input class="mybouton" type="submit" value="Inscrivez-vous !" onclick="run('inscription')" />
+                </div>
+
+            </form>
+        </div>
