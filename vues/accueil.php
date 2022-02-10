e3d5db55528b5eaec69b3e861f0632e21c1564a2
NoodMan <camara.adeline@yahoo.fr>
2022-02-10 11:05:12 +0100

    add accueil

--- vues/accueil.php
+++ vues/accueil.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
     <title>Page accueil</title>
 </head>
 <body>
@@ -159,16 +158,3 @@
  </html> 
 
 
-=======
-    <title>Accueil</title>
-</head>
-<body>
-  
-
-
-
-
-
-</body>
-</html>
->>>>>>> d358b18f17c68b78fa7a0810aa579a38032a2add
