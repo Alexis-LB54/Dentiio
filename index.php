973ccbf4d31d5e2b06f7bbbbbb7c900ef3d2b24e
NoodMan <camara.adeline@yahoo.fr>
2022-02-10 19:03:54 +0100

    modif style

--- index.php
+++ index.php
@@ -17,8 +17,8 @@
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
- 
-    <style>
+
+    <!-- <style>
         body {
             background-repeat: no-repeat;
             background-image: url(assets/images/illus.png);
@@ -121,12 +121,12 @@
 
             </form>
         </div> -->
-    </div>
+    <!-- </div>
 
-</body>
+</body> -->
 
 
-<script>
+    <!-- <script>
 
 function run(string $param) {
     $.ajax({
@@ -135,9 +135,67 @@ function run(string $param) {
 }
 
 
-</script>
+</script> -->
 
-    </form> 
- 
+    <!-- </form>  -->
 
 
+    <!-- noublie pas de enlever les commentaire avec de faire un truc sur githut!! -->
+
+
+
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Page favoris</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous" />
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js" integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+
+
+    </head>
+
+    <body>
+
+
+
+        <body>
+
+            <nav class="navbar navbar-expand-lg navbar-light " style="background-color:#00D1ED; height:115px;">
+                <div class="container-fluid">
+
+                    <div class="flex-avatar">
+
+                        <a href="google.fr"><img src="../../assets/images/avatar.png" style="height:70px;margin-right:40px;margin-left:20px;"></a>
+                        <a href="google.fr"><img src="../../assets/images/home.png" style="height:60px; margin:20px;"></a>
+                        <a href="google.fr"><img src="../../assets/images/star.png" style="height:60px;margin:20px;"></a>
+                        <a href="google.fr"><img src="../../assets/images/bell.ico" style="height:60px;margin:20px;"></a>
+
+                    </div>
+
+                    <div class="flex-logo">
+
+                        <img src="../../assets/images/logoBlancComplet.png" style="height:100px;">
+
+                    </div>
+
+                    <div class="flex-add">
+
+                        <a href="google.fr"> <img src="../../assets/images/boutonadd.png" style="height:60px; margin:20px"></a>
+
+                    </div>
+
+
+                </div>
+
+            </nav>
+
+
+
+           
--- vues/favori.php
+++ vues/favori.php
@@ -1,74 +1,89 @@
+<!DOCTYPE html>
+<html lang="fr">
 
-    <!DOCTYPE html>
-    <html lang="en">
-
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Page favoris</title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous" />
-        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js" integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-
-
-    </head>
-
-    <body>
-
-        <style>
-            body {
-                margin-left: 20px;
-
-  margin-top: 20px;
-                
-            }
-
-            h1 {
-                color: #03B6F0;
-                text-align: center;
-            }
-
-            .container {
-                display: block;
-            }
-               
-        </style>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Page favoris</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous" />
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js" integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 
 
-        <h1><img src="https://img.icons8.com/ios/50/4a90e2/likes-folder--v1.png" /></i> VOS FAVORIS</h1>
+</head>
 
-&nbsp;
+<body>
 
-&nbsp;
+  <style>
+    body {
+      margin-top: auto;
+    }
 
+    h1 {
+      color: #03B6F0;
+      text-align: center;
+    }
 
-<div class="d-flex justify-content-evenly">
-<div class="card" style="width: 18rem;">
-  <img src="/assets/images/dent10.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    .container {
+      display: block;
+    }
+
+    #img2 {
+      height: 35px;
+    }
+
+    #taille {
+      height: 335px;
+    }
+  </style>
+  <br>
+  <br>
+  <br>
+  <h1><img src="https://img.icons8.com/ios/50/4a90e2/likes-folder--v1.png" /></i> VOS FAVORIS</h1>
+
+  &nbsp;
+  <br>
+  &nbsp;
+  <br>
+
+  <div class="d-flex justify-content-evenly">
+    <div class="card border border-info" style="width: 20rem;">
+      <img id="taille" src="/assets/images/dent beurk.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+      <div class="card-footer d-flex justify-content-around">
+        <img id="img2" src="/assets/images/dent2.jpg" alt="..." class="rounded-circle">
+        <small class="text-muted">Last 3 mins ago</small> &nbsp;
+        <img id="img2" src="https://img.icons8.com/external-kiranshastry-lineal-kiranshastry/64/4a90e2/external-tooth-dental-kiranshastry-lineal-kiranshastry-2.png" />
+        <img id="img2" src="https://img.icons8.com/ios/50/4a90e2/speech-bubble--v1.png" />
+      </div>
+    </div>
+
+    <div class="card border border-info" style="width: 18rem;">
+      <img id="taille" src="/assets/images/dent selfie.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+      <div class="card-footer d-flex justify-content-around">
+        <img id="img2" src="/assets/images/dent2.jpg" alt="..." class="rounded-circle">
+        <small class="text-muted">Last 3 mins ago</small> &nbsp;
+        <img id="img2" src="https://img.icons8.com/external-kiranshastry-lineal-kiranshastry/64/4a90e2/external-tooth-dental-kiranshastry-lineal-kiranshastry-2.png" />
+        <img id="img2" src="https://img.icons8.com/ios/50/4a90e2/speech-bubble--v1.png" />
+      </div>
+    </div>
   </div>
-</div>
-
-<div class="card" style="width: 18rem;">
-  <img src="/assets/images/dent selfie.png" class="card-img-top" alt="...">
-  <div class="card-body">
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
   </div>
-</div>
-</div>
-</div>
 
-&nbsp;
+  &nbsp;
 
 
 
+</body>
 
-
-
-    </body>
-
-    </html>
+</html>
