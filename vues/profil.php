3fbe2b8bc48733abded4b55bdc9a69e5e70797a6
Alexis-LB54 <alexis@lebail.eu>
2022-02-11 14:17:49 +0100

    commit vendredi aprem

--- vues/profil.php
+++ vues/profil.php
@@ -12,79 +12,112 @@
 
     <?php include './templates/header.php' ?>
 
-    <div class="profilcorps">
+    <div class="profilcorps" >
 
-        <div class="container" id="headerprofil">
-            <div class="profilphoto">
-                <img src="/assets/images/pp1.png" width="150" height="150">
+        <div class="contenant">
+
+            <!-- <div id="headerprofil"> -->
+
+            <!-- <div class="header2"> -->
+
+            <div>
+                <img class="image_profil" src="/assets/images/pp1.png" width="150" height="150">
             </div>
 
-            <div class="profilIS">
-                <div class="profilinfo">
-                    <div class="profilname">
-                        Sam
-                    </div>
-
-                    <div class="job">
-                        Totally Spies chez le WHOOP
-                    </div>
+            <div>
+                <div>
+                    <h1>Sam</hi>
                 </div>
 
-                <div class="setting">
-                    <div class="modification">
-                        <a href="./profil.php">Modifier vos informations</a>
-
-                    </div>
-                    <div class="déconnexion">
-                        <a href="/index.php">Se déconnecter</a>
-                    </div>
+                <div>
+                    <p class="job">Totally Spies chez le WHOOP</p>
                 </div>
             </div>
+
+            <div class="setting">
+
+                <div>
+                    <input href="./profil.php" class="modif_et_deco" type="button" value="Modifier vos informations">
+                </div>
+
+                <div>
+                    <input href="./index.php" class="modif_et_deco" type="button" value="Se deconnecter">
+                </div>
+
+            </div>
+
         </div>
 
+        <div id="fonctionprofile">
 
-
-        <div class="container" id="fonctionprofile">
-            <div class="publication">
+            <div class="boutons_profile">
                 Publication
             </div>
-            <div class="discussion">
+
+            <div class="boutons_profile">
                 Discusion
             </div>
+
         </div>
 
-        <div class="container" id="imagepublication">
-            <div class="row row-cols-1 row-cols-md-3 g-4">
-                <div class="image1">
-                    <div class="col">
-                        <div class="card h-100" id="divprofil1">
-                            <img id="imageprofil1" src="/assets/images/dent12.png" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text" id="textprofil1"  >This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            </div>
-                            <div class="card-footer">
-                                <small class="text-muted">Last updated 3 mins ago</small>
-                            </div>  
-                        </div>
-                    </div>
+
+
+        <div id="imagepublication">
+
+<div class="row row-cols-1 row-cols-md-2 g-4">
+
+    <div class="">
+        <div class="">
+            <div class="card-body">
+
+                <img id="imageprofil1" src="/assets/images/dent12.png" class="card-img-top" alt="...">
+
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text" id="textprofil1">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                 </div>
-                <div class="image2">
-                    <div class="col">
-                        <div class="card h-100" id="divprofil2">
-                            <img id="imageprofil2" src="/assets/images/dent11.png" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text" id="textprofil2" >This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                            </div>
-                            <div class="card-footer">
-                                <small class="text-muted">Last updated 3 mins ago</small>
-                            </div>
-                        </div>
-                    </div>
+
+                <div class="card-footer">
+                    <small class="text-muted">Last updated 3 mins ago</small>
                 </div>
+
             </div>
         </div>
+    </div>
+
+    <div class="">
+        <div class="">
+            <div class="card-body">
+
+                <img id="imageprofil1" src="/assets/images/dent12.png" class="card-img-top" alt="...">
+
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text" id="textprofil1">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+
+                <div class="card-footer">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    
+</div>
+
+</div>
+
+
+
+    </div>
+
+    </div>
+
+
+
+
+    </div>
 
     </div>
 
